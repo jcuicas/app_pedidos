247dfd33e5207a685e06e77f6cdafbfbb6a3d750
jcuicas <josecuicas@gmail.com>
2024-09-07 00:56:55 -0400

    decimo segundo commit

--- lib/screens/orden_pedidos.dart
+++ lib/screens/orden_pedidos.dart
@@ -3,12 +3,17 @@ import 'package:app_pedidos/components/my_button_drawer.dart';
 import 'package:app_pedidos/components/my_drawer.dart';
 import 'package:app_pedidos/inherited/my_inherited.dart';
 import 'package:app_pedidos/models/cliente.dart';
+import 'package:app_pedidos/models/productos.dart';
 import 'package:app_pedidos/providers/obtener_datos_clientes.dart';
+import 'package:app_pedidos/providers/obtener_datos_existencias.dart';
 import 'package:app_pedidos/providers_off/obtener_datos_clientes_off.dart';
+import 'package:app_pedidos/providers_off/obtener_datos_existencias_off.dart';
 import 'package:app_pedidos/screens/ver_pedidos.dart';
 import 'package:app_pedidos/storage/client_storage.dart';
+import 'package:app_pedidos/storage/product_storage.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class OrdenPedido extends StatefulWidget {
   const OrdenPedido({super.key});
@@ -20,17 +25,30 @@ class OrdenPedido extends StatefulWidget {
 class _OrdenPedidoState extends State<OrdenPedido> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  var maskCantidad = MaskTextInputFormatter(
+    mask: '#####',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy,
+  );
+
   Future<List<Cliente>>? listaClientes;
+  Future<List<Producto>>? listaProductos;
 
   final clientStorage = ClientStorage();
+  final productStorage = ProductStorage();
 
   String? selectedValueClient;
+  String? selectedValueProduct;
 
   final textEditingControllerClient = TextEditingController();
+  final textEditingControllerProduct = TextEditingController();
+  final textEditingControllerCantidad = TextEditingController();
 
   @override
   void dispose() {
     textEditingControllerClient.dispose();
+    textEditingControllerProduct.dispose();
+    textEditingControllerCantidad.dispose();
 
     super.dispose();
   }
@@ -39,8 +57,10 @@ class _OrdenPedidoState extends State<OrdenPedido> {
   Widget build(BuildContext context) {
     if (GetInfoUser.of(context).conexion!) {
       listaClientes = obtenerClientes();
+      listaProductos = obtenerProductos();
     } else {
       listaClientes = obtenerClientesLocal();
+      listaProductos = obtenerProductosLocal();
     }
 
     return Scaffold(
@@ -133,6 +153,7 @@ class _OrdenPedidoState extends State<OrdenPedido> {
                             left: 8,
                           ),
                           child: TextFormField(
+                            textCapitalization: TextCapitalization.characters,
                             expands: true,
                             maxLines: null,
                             controller: textEditingControllerClient,
@@ -173,6 +194,189 @@ class _OrdenPedidoState extends State<OrdenPedido> {
                 child: CircularProgressIndicator(),
               );
             },
+          ),
+          /* PRODUCTOS */
+          FutureBuilder(
+            future: listaProductos,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Container(
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton2<String>(
+                      isExpanded: true,
+                      hint: Text(
+                        'Seleccionar producto',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Theme.of(context).hintColor,
+                        ),
+                      ),
+                      items: snapshot.data! //items
+                          .map((item) => DropdownMenuItem(
+                                value: '${item.producto} - ${item.id}',
+                                child: Text(
+                                  item.producto,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ))
+                          .toList(),
+                      value: selectedValueProduct,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedValueProduct = value;
+                        });
+                      },
+                      buttonStyleData: const ButtonStyleData(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        height: 40,
+                        width: 200,
+                      ),
+                      dropdownStyleData: const DropdownStyleData(
+                        maxHeight: 200,
+                      ),
+                      menuItemStyleData: const MenuItemStyleData(
+                        height: 40,
+                      ),
+                      dropdownSearchData: DropdownSearchData(
+                        searchController: textEditingControllerProduct,
+                        searchInnerWidgetHeight: 50,
+                        searchInnerWidget: Container(
+                          height: 50,
+                          padding: const EdgeInsets.only(
+                            top: 8,
+                            bottom: 4,
+                            right: 8,
+                            left: 8,
+                          ),
+                          child: TextFormField(
+                            textCapitalization: TextCapitalization.characters,
+                            expands: true,
+                            maxLines: null,
+                            controller: textEditingControllerProduct,
+                            decoration: InputDecoration(
+                              isDense: true,
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 8,
+                              ),
+                              hintText: 'Buscar un producto...',
+                              hintStyle: const TextStyle(fontSize: 12),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                          ),
+                        ),
+                        searchMatchFn: (item, searchValue) {
+                          return item.value.toString().contains(searchValue);
+                        },
+                      ),
+                      //This to clear the search value when you close the menu
+                      onMenuStateChange: (isOpen) {
+                        if (!isOpen) {
+                          textEditingControllerProduct.clear();
+                        }
+                      },
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('No hay datos de productos...'),
+                );
+              }
+
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 6.0,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    child: Text('Precio x unidad: '),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 6.0,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(
+                      color: Colors.red,
+                    ))),
+                    child: Text('Existencia: '),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 6.0,
+                  ),
+                  child: TextFormField(
+                    controller: textEditingControllerCantidad,
+                    inputFormatters: [maskCantidad],
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.red,
+                      )),
+                      hintText: 'Cantidad',
+                      hintStyle: TextStyle(fontSize: 14.0),
+                      isDense: true,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 6.0,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(
+                      color: Colors.red,
+                    ))),
+                    child: Text('Subtotal: '),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
